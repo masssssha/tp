163b1472de174064376b5ff0df59f623b321801b
masssssha <mehovamaria@yandex.ru>
2024-11-30 21:35:24 +0300

    delete metrics_2d

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -1,105 +1,98 @@
 import numpy as np
+import torch
 import csv
-import matplotlib.pyplot as plt
 import cv2
 import time
+import pytorch_ssim
+import matplotlib.pyplot as plt
 import os
-import torch
-from pytorch_ssim import ssim
 
-folder_with_data = '/home/masha/validate.vol'
-data_id = '/home/masha/number.txt'
-path_to_save = '/home/masha/tp/results'
-header = ['radius', 'low', 'clinical']
-rad_circle_start = 100
-rad_circle_end = 182
-radius_circle = [i for i in range(rad_circle_start, rad_circle_end + 1, 2)]
-rad_square_start = 70
-rad_square_end = 128
-radius_square = [i for i in range(rad_square_start, rad_square_end + 1, 2)]
-func_list = ['MASSIM']
-
-class ImError(Exception):
+class ImError():
     def __init__(self, shape_1, shape_2, shape_3):
         self.shape_1 = shape_1
         self.shape_2 = shape_2
         self.shape_3 = shape_3
     def __str__(self):
-        return f"Invalid image sizes: {self.shape_1}, {self.shape_2}, {self.shape_3}."
-    
+        return f"Invalid image sizes: {self.shape_1}; {self.shape_2}; {self.shape_3}."
+
+folder_with_data = '/home/masha/validate.vol'
+data_id = '/home/masha/number.txt'
+path_to_save = '/home/masha/results_float64/results_3_float64'
+header = ['radius', 'low', 'clinical']
+radius_circle_start = 100
+radius_circle_end = 182
+radius_circle = [i for i in range(radius_circle_start, radius_circle_end + 1, 2)]
+wing_square_start = 70
+wing_square_end = 128
+radius_square = [i for i in range(wing_square_start, wing_square_end + 1, 2)]
+func_list = ['MAMSE', 'MASSIM', 'MASTRESS', 'MANRMSD']
+
 def MAMSE(phantom: np.ndarray, recon: np.ndarray, mask: np.ndarray = None) -> np.float32:
-    """Calculate Masked Mean Squared Error.
+    """Calculate masked mean squared error.
     
     Parameters:
-        phantom : np.ndarray, shape (n, n))
+        phantom : 3-d np.ndarray
             Correct values.
 
-        recon : np.ndarray, shape (n, n)
+        recon : 3-d np.ndarray
             Estimated values.
 
-        mask : np.ndarray
-            Mask by which the MAMSE should be calculated. 
-            If None, a new mask will be created according to np.shape(phantom).
+        mask : 2-d or 3-d np.ndarray
+            The mask by which MAMSE is calculated.
+
     Returns:
         value : np.float32 
             Masked mean squared error (the best value is 0.0).
-    """
+    """  
     if mask is None:
         mask = np.ones(np.shape(phantom))
+    if mask.ndim == 2:
+        mask = np.ones(np.shape(phantom))*mask.copy()
     if np.shape(phantom) != np.shape(recon) or np.shape(phantom) != np.shape(mask):
         raise ImError(np.shape(phantom), np.shape(recon), np.shape(mask))
-    img_1 = phantom.copy()
-    img_2 = recon.copy()
-    img_1[mask == 0] = 0
-    img_2[mask == 0] = 0
-    return np.sum((img_1 - img_2)**2)/np.nonzero(mask)
 
-def normalized(ref: np.ndarray, tr_low: np.float32 = -0.2, tr_hi: np.float32 = 3.5) -> np.ndarray:
-    img = ref.copy()
-    img = (img - tr_low)/(tr_hi - tr_low)
-    img = np.where(img < 0.0, 0.0, img)
-    img = np.where(img > 1.0, 1.0, img)
-    return img
+    vol_1 = phantom.copy()
+    vol_2 = recon.copy()
+    vol_1[mask == 0] = 0
+    vol_2[mask == 0] = 0
+    return np.sum((vol_1 - vol_2)**2)/np.count_nonzero(mask)
 
 def make_tensor(img: np.ndarray) -> torch.Tensor:
-    temp = np.zeros((1, 1, np.shape(img)[0], np.shape(img)[1]))
-    temp[0, 0, :, :] = img.copy()
+    temp = np.zeros((np.shape(img)[0], 1, np.shape(img)[1], np.shape(img)[2]))
+    temp[:, 0, :, :] = img.copy()
     return torch.Tensor(np.array(temp))
 
-
-def MASSIM(phantom: np.ndarray, recon: np.ndarray, mask: np.ndarray = None):
-    """Calculate Masked Structural Similarity Index Measure.
+def MASSIM(phantom: np.ndarray, recon: np.ndarray, mask: np.ndarray = None) -> np.float32:
+    """Calculate masked SSIM.
     
     Parameters:
-        phantom : np.ndarray, shape (n, n)
+        phantom : np.ndarray, shape (n, n), data_range=(0.0, 1.0)
             Correct values.
 
-        recon : np.ndarray, shape (n, n)
+        recon : np.ndarray, shape (n, n), data_range=(0.0, 1.0)
             Estimated values.
 
-        mask : np.ndarray
-            Mask by which the MASSIM should be calculated. 
-            If None, a new mask will be created according to np.shape(phantom).
+        mask : np.ndarray, shape (n, n), data_range=(0.0, 1.0)
+            The mask by which MASSIM is calculated.
+
     Returns:
         value : np.float32 
-            Masked Structural Similarity Index Measure (the best value is 1.0).
+            Masked SSIM (the best value is 1.0).
     """
     if mask is None:
         mask = np.ones(np.shape(phantom))
+    if mask.ndim == 2:
+        mask = np.ones(np.shape(phantom))*mask.copy()
     if np.shape(phantom) != np.shape(recon) or np.shape(phantom) != np.shape(mask):
         raise ImError(np.shape(phantom), np.shape(recon), np.shape(mask))
-    img_1 = phantom.copy()
-    img_2 = recon.copy()
-    mask_copy = mask.copy()
-
-    img_1 = make_tensor(img_1)
-    img_2 = make_tensor(img_2)
-    mask_copy = make_tensor(mask_copy)
-    mask_copy = mask_copy.bool()
-    value = ssim(img_1, img_2, 1.0, mask=mask_copy)
+    img_1 = make_tensor(phantom)
+    img_2 = make_tensor(recon)
+    mask = make_tensor(mask)
+    mask = mask.bool()
+    value = pytorch_ssim.ssim(img_1, img_2, data_range=1.0, mask=mask)
     return value.item()
 
-def MASTRESS(recon: np.ndarray, phantom: np.ndarray, mask: np.ndarray = None):
+def MASTRESS(recon: np.ndarray, phantom: np.ndarray, mask: np.ndarray = None, per_slice_avg: bool = False):
     """Calculate Masked standardised residual sum of squares.
     
     Parameters:
@@ -117,22 +110,38 @@ def MASTRESS(recon: np.ndarray, phantom: np.ndarray, mask: np.ndarray = None):
     """
     if mask is None:
         mask = np.ones(np.shape(phantom))
+    if mask.ndim == 2:
+        mask = np.ones(np.shape(phantom))*mask.copy()
     if np.shape(phantom) != np.shape(recon) or np.shape(phantom) != np.shape(mask):
         raise ImError(np.shape(phantom), np.shape(recon), np.shape(mask))
     img_1 = phantom.copy()
     img_2 = recon.copy()
     img_1[mask == 0] = 0
     img_2[mask == 0] = 0
-    dot_prod = np.sum((img_1 * img_2))
-    recon_l2 = np.sum(img_2 ** 2)
-    phantom_l2 = np.sum(img_1 ** 2)
-    result = dot_prod / (recon_l2 * phantom_l2) ** 0.5
-    result *= result
-    result = 1 - result
-    result = result ** 0.5
+    result = 0
+    if per_slice_avg:
+        n_slices = np.shape(phantom)[0]
+        for i in range(n_slices):
+            dot_prod = np.sum((img_1[i] * img_2[i]))
+            recon_l2 = np.sum(img_2[i] ** 2)
+            phantom_l2 = np.sum(img_1[i] ** 2)
+            value = dot_prod / (recon_l2 * phantom_l2) ** 0.5
+            value *= value
+            value = 1 - value
+            value = value ** 0.5
+            result += value/n_slices
+    else:
+        dot_prod = np.sum((img_1 * img_2))
+        recon_l2 = np.sum(img_2 ** 2)
+        phantom_l2 = np.sum(img_1 ** 2)
+        result = dot_prod / (recon_l2 * phantom_l2) ** 0.5
+        result *= result
+        result = 1 - result
+        result = result ** 0.5
     return result
 
-def MANRMSD(recon: np.ndarray, phantom: np.ndarray, mask: np.ndarray = None):
+        
+def MANRMSD(recon: np.ndarray, phantom: np.ndarray, mask: np.ndarray = None, per_slice_avg: bool = False):
     """Calculate Masked normalized root mean squared difference.
     
     Parameters:
@@ -150,111 +159,121 @@ def MANRMSD(recon: np.ndarray, phantom: np.ndarray, mask: np.ndarray = None):
     """
     if mask is None:
         mask = np.ones(np.shape(phantom))
+    if mask.ndim == 2:
+        mask = np.ones(np.shape(phantom))*mask.copy()
     if np.shape(phantom) != np.shape(recon) or np.shape(phantom) != np.shape(mask):
         raise ImError(np.shape(phantom), np.shape(recon), np.shape(mask))
     img_1 = phantom.copy()
     img_2 = recon.copy()
     img_1[mask == 0] = 0
     img_2[mask == 0] = 0
-    diff_sqr = np.zeros((recon.shape[0], recon.shape[1]))
-    diff_sqr = (img_2 - img_1) ** 2
-    nrmsd = (np.sum(diff_sqr) / np.sum(img_1 ** 2)) ** 0.5
+    nrmsd = 0
+    if per_slice_avg:
+        n_slices = np.shape(phantom)[0]
+        for i in range(n_slices):
+            diff_sqr = (img_2[i] - img_1[i]) ** 2
+            value = (np.sum(diff_sqr) / np.sum(img_1[i] ** 2)) ** 0.5
+            nrmsd += value/n_slices
+    else:
+        diff_sqr = (img_2 - img_1) ** 2
+        nrmsd = (np.sum(diff_sqr) / np.sum(img_1 ** 2)) ** 0.5
     return nrmsd
 
-func_dict = {'MAMSE': MAMSE, 'MASSIM': MASSIM, 'MASTRESS': MASTRESS, 'MANRMSD': MANRMSD}
+func_dict = {'MAMSE': MAMSE, 'MASSIM': MASSIM, 'MASTRESS': MASTRESS,
+             'MANRMSD': MANRMSD}
 
-def create_circle_mask(radius: int) -> np.ndarray:
-    mask = cv2.circle(np.zeros((256, 256)), (128, 128), radius, (1), -1)
-    return mask
+def normalized(ref: np.ndarray, tr_low: np.float32 = -0.2, tr_hi: np.float32 = 3.5) -> np.ndarray:
+    img = ref.copy()
+    img = (img - tr_low)/(tr_hi - tr_low)
+    img = np.where(img < 0.0, 0.0, img)
+    img = np.where(img > 1.0, 1.0, img)
+    return img
+
+def save_table(table: list[list], filename: str) -> None:
+    with open(f'{filename}.tsv', 'w') as file:
+        writer = csv.writer(file, delimiter='\t')
+        writer.writerows(table)
 
 def create_square_mask(radius: int) -> np.ndarray:
-    mask = cv2.rectangle(np.zeros((256, 256)), (128-radius, 128-radius), 
-                         (127+radius, 127+radius), (1), -1)
+    mask = cv2.rectangle(np.zeros((256, 256), dtype=np.float64), (128-radius, 128-radius), (127+radius, 127+radius), (1), -1)
+    return mask
+
+def create_circle_mask(radius: int) -> np.ndarray:
+    mask = cv2.circle(np.zeros((256, 256), dtype=np.float64), (128, 128), radius, (1), -1)
     return mask
 
 def create_crop(ref: np.ndarray, radius: int) -> np.ndarray:
     img = ref.copy()
-    return img[(128-radius):(128+radius), (128-radius):(128+radius)]
-
-def save_table(table: list[list], filename: str, path_to_save: str = None) -> None:
-    if path_to_save is None:
-        with open(f'{filename}.tsv', 'w') as file:
-            writer = csv.writer(file, delimiter='\t')
-            writer.writerows(table)
-    else:
-        with open(f'{path_to_save}/{filename}.tsv', 'w') as file:
-            writer = csv.writer(file, delimiter='\t')
-            writer.writerows(table)
+    return img[:, (128-radius):(128+radius), (128-radius):(128+radius)]
 
 def calculate_statistics_circle(func_lst: list[str], vol_ref: np.ndarray, 
                         vol_low: np.ndarray, vol_clinical: np.ndarray, mask: list, id: int) -> None:
     tables = []
-    slices = np.shape(vol_ref)[0]
     for i in range(len(func_lst)):
         tab = [[0 for j in range(3)] for k in range(len(mask)+1)]
         tab[0] = header
         tables.append(tab)
     for rad_id in range(len(mask)):
-        for n_slice in range(slices):
-            for i in range(len(func_lst)):
-                if func_lst[i] == 'MASSIM':
-                    img_1 = normalized(vol_ref[n_slice])
-                    img_2 = normalized(vol_low[n_slice])
-                    img_3 = normalized(vol_clinical[n_slice])
-                else:
-                    img_1 = vol_ref[n_slice]
-                    img_2 = vol_low[n_slice]
-                    img_3 = vol_clinical[n_slice]
-                tables[i][rad_id+1][0] = radius_circle[rad_id]
-                tables[i][rad_id+1][1] += (func_dict[func_lst[i]](img_1, img_2, mask[rad_id]))/slices
-                tables[i][rad_id+1][2] += (func_dict[func_lst[i]](img_1, img_3, mask[rad_id]))/slices
-
+        for i in range(len(func_lst)):
+            if func_lst[i] == 'MASSIM':
+                img_1 = normalized(vol_ref)
+                img_2 = normalized(vol_low)
+                img_3 = normalized(vol_clinical)
+            else:
+                img_1 = vol_ref
+                img_2 = vol_low
+                img_3 = vol_clinical
+            tables[i][rad_id+1][0] = radius_circle[rad_id]
+            tables[i][rad_id+1][1] = (func_dict[func_lst[i]](img_1, img_2, mask[rad_id]))
+            tables[i][rad_id+1][2] = (func_dict[func_lst[i]](img_1, img_3, mask[rad_id]))
+        print(f'Finished {radius_circle[rad_id]} (circle)', time.ctime())
     for i in range(len(func_lst)):
         if not os.path.isdir(f'{path_to_save}/{func_lst[i]}_circle'):
             os.mkdir(f'{path_to_save}/{func_lst[i]}_circle')
-        save_table(tables[i], f'{path_to_save}/{func_lst[i]}_circle/{func_lst[i]}_{id}')
+        save_table(tables[i], f'{path_to_save}/{func_lst[i]}_circle/{func_lst[i]}_circle_{id}')
+
+
 
 def calculate_statistics_square_crop(func_lst: list[str], vol_ref: np.ndarray, 
                         vol_low: np.ndarray, vol_clinical: np.ndarray, mask: list, id: int) -> None:
-    tables_square = []
+    #tables_square = []
     tables_crop = []
-    slices = np.shape(vol_ref)[0]
     for i in range(len(func_lst)):
-        tab = [[0 for j in range(3)] for k in range(len(mask)+1)]
+        '''tab = [[0 for j in range(3)] for k in range(len(mask)+1)]
         tab[0] = header
-        tables_square.append(tab)
+        tables_square.append(tab)'''
         tab = [[0 for j in range(3)] for k in range(len(mask)+1)]
         tab[0] = header
         tables_crop.append(tab)
     for rad_id in range(len(mask)):
-        for n_slice in range(slices):
-            for i in range(len(func_lst)):
-                if func_lst[i] == 'MASSIM':
-                    img_1 = normalized(vol_ref[n_slice])
-                    img_2 = normalized(vol_low[n_slice])
-                    img_3 = normalized(vol_clinical[n_slice])
-                else:
-                    img_1 = vol_ref[n_slice]
-                    img_2 = vol_low[n_slice]
-                    img_3 = vol_clinical[n_slice]
-                tables_square[i][rad_id+1][0] = radius_square[rad_id]
-                tables_square[i][rad_id+1][1] += (func_dict[func_lst[i]](img_1, img_2, mask[rad_id]))/slices
-                tables_square[i][rad_id+1][2] += (func_dict[func_lst[i]](img_1, img_3, mask[rad_id]))/slices
-                crop_1 = create_crop(img_1, radius_square[rad_id])
-                crop_2 = create_crop(img_2, radius_square[rad_id])
-                crop_3 = create_crop(img_3, radius_square[rad_id])
+        for i in range(len(func_lst)):
+            if func_lst[i] == 'MASSIM':
+                img_1 = normalized(vol_ref)
+                img_2 = normalized(vol_low)
+                img_3 = normalized(vol_clinical)
+            else:
+                img_1 = vol_ref
+                img_2 = vol_low
+                img_3 = vol_clinical
+            '''tables_square[i][rad_id+1][0] = radius_square[rad_id]
+            tables_square[i][rad_id+1][1] = (func_dict[func_lst[i]](img_1, img_2, mask[rad_id]))
+            tables_square[i][rad_id+1][2] = (func_dict[func_lst[i]](img_1, img_3, mask[rad_id]))'''
+            crop_1 = create_crop(img_1, radius_square[rad_id])
+            crop_2 = create_crop(img_2, radius_square[rad_id])
+            crop_3 = create_crop(img_3, radius_square[rad_id])
 
-                tables_crop[i][rad_id+1][0] = radius_square[rad_id]
-                tables_crop[i][rad_id+1][1] += (func_dict[func_lst[i]](crop_1, crop_2))/slices
-                tables_crop[i][rad_id+1][2] += (func_dict[func_lst[i]](crop_1, crop_3))/slices
-
+            tables_crop[i][rad_id+1][0] = radius_square[rad_id]
+            tables_crop[i][rad_id+1][1] = (func_dict[func_lst[i]](crop_1, crop_2))
+            tables_crop[i][rad_id+1][2] = (func_dict[func_lst[i]](crop_1, crop_3))
+        print(f'Finished {radius_square[rad_id]} (square)', time.ctime())
     for i in range(len(func_lst)):
-        if not os.path.isdir(f'{path_to_save}/{func_lst[i]}_square'):
+        '''if not os.path.isdir(f'{path_to_save}/{func_lst[i]}_square'):
             os.mkdir(f'{path_to_save}/{func_lst[i]}_square')
-        save_table(tables_square[i], f'{path_to_save}/{func_lst[i]}_square/{func_lst[i]}_{id}')
+        save_table(tables_square[i], f'{path_to_save}/{func_lst[i]}_square/{func_lst[i]}_square_{id}')'''
         if not os.path.isdir(f'{path_to_save}/{func_lst[i]}_crop'):
             os.mkdir(f'{path_to_save}/{func_lst[i]}_crop')
-        save_table(tables_crop[i], f'{path_to_save}/{func_lst[i]}_crop/{func_lst[i]}_{id}')
+        save_table(tables_crop[i], f'{path_to_save}/{func_lst[i]}_crop/{func_lst[i]}_crop_{id}')
+
 
 def main():
     with open(data_id, 'r') as file:
@@ -264,21 +283,21 @@ def main():
         print(f'Started {content[id]}', time.ctime())
 
         #load data
-        clean_vol = np.load(f'{folder_with_data}/{content[id]}_clean_fdk_256.npy')
-        fdk_low_dose = np.load(f'{folder_with_data}/{content[id]}_fdk_low_dose_256.npy')
-        fdk_clinical_dose = np.load(f'{folder_with_data}/{content[id]}_fdk_clinical_dose_256.npy')
+        clean_vol = np.float64(np.load(f'{folder_with_data}/{content[id]}_clean_fdk_256.npy'))
+        fdk_low_dose = np.float64(np.load(f'{folder_with_data}/{content[id]}_fdk_low_dose_256.npy'))
+        fdk_clinical_dose = np.float64(np.load(f'{folder_with_data}/{content[id]}_fdk_clinical_dose_256.npy'))
 
         #create masks
         circle_masks = []
-        for j in range(rad_circle_start, rad_circle_end+1, 2):
+        for j in range(radius_circle_start, radius_circle_end+1, 2):
             circle_masks.append(create_circle_mask(j))
 
         square_masks = []
-        for j in range(rad_square_start, rad_square_end+1, 2):
+        for j in range(wing_square_start, wing_square_end+1, 2):
             square_masks.append(create_square_mask(j))
         
-        calculate_statistics_circle(func_list, clean_vol, fdk_low_dose, fdk_clinical_dose, circle_masks, 801+id)
-        print('Calculated circle', time.ctime())
+        #calculate_statistics_circle(func_list, clean_vol, fdk_low_dose, fdk_clinical_dose, circle_masks, 801+id)
+        #print('Calculated circle', time.ctime())
         calculate_statistics_square_crop(func_list, clean_vol, fdk_low_dose, fdk_clinical_dose, square_masks, 801+id)
         print(f'Finished {content[id]}', time.ctime())
     
--- scripts/test.py
+++ scripts/test.py
@@ -1,27 +1,32 @@
+import metrics
 import numpy as np
-from metrics import *
+import matplotlib.pyplot as plt
 import pytest
-from vizualize import vizualize
-from os.path import dirname
-import sys
 
 def create_reference(radius: int):
-    img_1 = np.zeros((256, 256))
-    img_2 = np.zeros((256, 256))
-    img_2[128, 128] = 1
-    mask = create_square_mask(radius)
+    img_1 = np.ones((256, 256))
+    img_2 = np.ones((256, 256))
+    img_2[128, 128] = 0
+    mask = metrics.create_square_mask(radius)
     result = 1/((2*radius)**2)
     return img_1, img_2, mask, result
 
-def test_():
-    rad = 5
-    img_1, img_2, mask, res = create_reference(rad)
-    crop_mask = create_crop(mask, rad)
-    folder = f'{dirname(dirname(sys.argv[0]))}/images'
-    assert np.count_nonzero(crop_mask) == (rad*2)**2
-    vizualize(mask, 'Mask for test', path_to_save=folder)
-    vizualize(crop_mask, 'Crop of mask', path_to_save=folder)
-    with pytest.raises(ImError): 
-        MAMSE(img_1, img_2, np.ones((255, 256)))
+def stress_ref():
+    img_1 = np.ones((2, 256, 256))
+    img_2 = np.ones((2, 256, 256))
+    img_2[:, 128, 128] = 0
+    img_2[1, 128, 127] = 0
+    return img_1, img_2
 
+def test_():
+    '''rad = 5
+    img_1, img_2, mask, res = create_reference(rad)
+    crop_mask = metrics.create_crop(mask, rad)
+    assert np.count_nonzero(crop_mask) == (rad*2)**2
+
+    mse = metrics.MAMSE(img_1, img_2, mask)
+    assert mse == res'''
+
+    img_3, img_4 = stress_ref()
+    print((metrics.MANRMSD(img_3, img_4, per_slice_avg=True)), metrics.MANRMSD(img_3, img_4))
 test_()
